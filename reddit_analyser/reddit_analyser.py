a764bc9b57d5b0e0e71cc6ade233a158d8ba631d
RuikisR <raivoruikis@gmail.com>
2021-11-30 16:24:38 +0000

    Added: Planning

--- reddit_analyser/reddit_analyser.py
+++ reddit_analyser/reddit_analyser.py
@@ -1,9 +1,41 @@
-import scraper
+from scraper import Scraper
+
+
+def get_data(use_local=True):
+    '''
+    Obtain the data for processing by either loading it from the local
+    directory or by creating a reddit scraper to fetch everything we need
+    '''
+    if not use_local:
+        scraper = Scraper()
+        scraper.pull_data()
+        scraper.dump_data()
+        return scraper.data
+    else:
+        return
+
+
+#########################################################################
+'''
+Place functions pertaining to the various ML approaches used here,
+as well as the functions defined to perform analysis on their performance
+'''
+
+
+def example_processing(data):
+    [print(line) for line in data]
+
+
+#########################################################################
 
 
 def main():
-    reddit = scraper.create_instance()
-    [print(line) for line in scraper.pull_data(reddit)]
+    '''
+    Should only be used to pass around the data to the various functions
+    which will process it
+    '''
+    data = get_data(use_local=False)
+    example_processing(data)
 
 
 if __name__ == '__main__':
--- reddit_analyser/scraper.py
+++ reddit_analyser/scraper.py
@@ -1,9 +1,20 @@
 import praw
 
 
-def create_instance():
-    return praw.Reddit("scraper", user_agent="scraper user agent")
+class Scraper:
+    def __init__(self):
+        self.instance = praw.Reddit("scraper", user_agent="scraper user agent")
 
+    def pull_data(self):
+        '''
+        Pulls the data we want from the Reddit API
+        (Will likely use a config file to control this)
+        '''
+        self.data = [post.title for post
+                     in self.instance.subreddit("dankmemes").hot(limit=10)]
 
-def pull_data(instance):
-    return [post.title for post in instance.subreddit("dankmemes").hot(limit=10)]
+    def dump_data(self):
+        '''
+        Used to dump the data into a csv file contained in the data/ directory
+        '''
+        pass

34a5b84d2534b65d96f8889d80327137145e8009
RuikisR <raivoruikis@gmail.com>
2021-11-30 16:45:46 +0000

    Added: Black for formatting and corresponding Make recipe

--- reddit_analyser/scraper.py
+++ reddit_analyser/scraper.py
@@ -6,15 +6,15 @@ class Scraper:
         self.instance = praw.Reddit("scraper", user_agent="scraper user agent")
 
     def pull_data(self):
-        '''
+        """
         Pulls the data we want from the Reddit API
         (Will likely use a config file to control this)
-        '''
-        self.data = [post.title for post
-                     in self.instance.subreddit("dankmemes").hot(limit=10)]
+        """
+        data_gen = self.instance.subreddit("dankmemes").hot(limit=10)
+        self.data = [post.title for post in data_gen]
 
     def dump_data(self):
-        '''
+        """
         Used to dump the data into a csv file contained in the data/ directory
-        '''
+        """
         pass

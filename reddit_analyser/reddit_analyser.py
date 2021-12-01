81fc540033650df7108f407b8d766fd2cdba977d
RuikisR <raivoruikis@gmail.com>
2021-12-01 13:44:42 +0000

    Modified: Format of X data, added Y data to be obtained in a similar manner, fixed padding

--- reddit_analyser/reddit_analyser.py
+++ reddit_analyser/reddit_analyser.py
@@ -8,10 +8,10 @@ def get_data(use_local=True):
     directory or by creating a reddit scraper to fetch everything we need
     """
     if not use_local:
-        scraper = Scraper()
-        scraper.pull_data()
-        scraper.dump_data()
-        return scraper.data, scraper.dictionary
+        s = Scraper()
+        s.pull_data()
+        s.dump_data()
+        return s.x_data, s.y_data, s.dictionary
     else:
         return
 
@@ -23,10 +23,16 @@ as well as the functions defined to perform analysis on their performance
 """
 
 
-def example_processing(data, dictionary):
-    counts = [sum(data[:, i]) for i in range(len(data[0]))]
-    highest = max(counts)
-    print(f"Most popular word: {dictionary['ProgrammerHumor'][counts.index(highest)]} with {highest}")
+def example_processing(x_data, y_data, dictionary):
+    for sub in x_data.keys():
+        x = np.array(x_data[sub])
+        y = np.array([[val] for val in y_data[sub]])
+        print(len(x), len(y))
+        counts = [sum(x[:, i]) for i in range(len(x[0]))]
+        highest = max(counts)
+        print(
+            f"Most popular word: {dictionary[sub][counts.index(highest)]} with {highest}"
+        )
 
 
 #########################################################################
@@ -37,9 +43,8 @@ def main():
     Should only be used to pass around the data to the various functions
     which will process it
     """
-    data, dictionary = get_data(use_local=False)
-    data = np.array(data)
-    example_processing(data, dictionary)
+    x, y, d = get_data(use_local=False)
+    example_processing(x, y, d)
 
 
 if __name__ == "__main__":

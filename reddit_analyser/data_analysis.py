e47cbd29986c133588072f8b551e232bac0a120a
RedmondC <colinredmond@protonmail.com>
2021-12-02 13:23:07 +0000

    generalised model execution

--- reddit_analyser/data_analysis.py
+++ reddit_analyser/data_analysis.py
@@ -1,6 +1,7 @@
 """
 Module to contain Machine Learning functions
 """
+import logging
 from sklearn.linear_model import LogisticRegression, Lasso
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
@@ -8,7 +9,6 @@ from sklearn.metrics import RocCurveDisplay, auc, roc_curve
 from sklearn.dummy import DummyClassifier
 import yaml
 import numpy as np
-import logging
 
 
 class DataAnalyser:
@@ -30,69 +30,55 @@ class DataAnalyser:
                 format="%(asctime)s - %(levelname)s - %(message)s",
             )
 
-    def lasso_regression(self, x_data: dict, y_data: dict):
-        """
-        Function to perform Lasso Regression on our data per subreddit
-        """
+    def model_execution(self, x_data: dict, y_data: dict, model_name: str, model):
         for subreddit in self.config["subreddits"]:
             x_list = np.array(x_data[subreddit])
             y_list = np.array(y_data[subreddit])
             x_train, x_test, y_train, y_test = train_test_split(
                 x_list, y_list, test_size=self.const["test_size"]
             )
-            model = Lasso(alpha=self.const["lasso_alpha"])
             model.fit(x_train, y_train)
-            predicted_x = model.predict(x_test)
-            fpr, tpr, thresholds = roc_curve(y_test, predicted_x)
+            fpr, tpr, thresholds = roc_curve(y_test, model.predict(x_test))
             score = auc(fpr, tpr)
-            logging.info("Lasso Regression on subreddit %s scored: %s", subreddit, score)
+            logging.info("%s on subreddit %s scored: %s", model_name, subreddit, score)
+
+    def lasso_regression(self, x_data: dict, y_data: dict):
+        """
+        Function to perform Lasso Regression on our data per subreddit
+        """
+        self.model_execution(
+            x_data, y_data, "Lasso Regression", Lasso(alpha=self.const["lasso_alpha"])
+        )
 
     def logistic_regression(self, x_data: dict, y_data: dict) -> None:
         """
         Function to perform Logistic Regression on our data per subreddit
         """
-        for subreddit in self.config["subreddits"]:
-            x_list = np.array(x_data[subreddit])
-            y_list = np.array(y_data[subreddit])
-            x_train, x_test, y_train, y_test = train_test_split(
-                x_list, y_list, test_size=self.const["test_size"]
-            )
-            model = LogisticRegression()
-            model.fit(x_train, y_train)
-            predicted_x = model.predict(x_test)
-            fpr, tpr, thresholds = roc_curve(y_test, predicted_x)
-            score = auc(fpr, tpr)
-            logging.info("Logistic Regression on subreddit %s scored: %s", subreddit, score)
+        self.model_execution(
+            x_data, y_data, "Logistic Regression", LogisticRegression()
+        )
 
     def k_neighbors(self, x_data: dict, y_data: dict) -> None:
         """
         Function to perform K Neighbors on our data per subreddit
         """
-        for subreddit in self.config["subreddits"]:
-            x_list = np.array(x_data[subreddit])
-            y_list = np.array(y_data[subreddit])
-            x_train, x_test, y_train, y_test = train_test_split(
-                x_list, y_list, test_size=self.const["test_size"]
-            )
-            model = KNeighborsClassifier(n_neighbors=self.const["K_val"])
-            model.fit(x_train, y_train)
-            score = cross_val_score(model, x_test, y_test, cv=self.const["cross_val"])
-            logging.info("K Nearest Neighbours on subreddit %s scored: %s", subreddit, score)
+        self.model_execution(
+            x_data,
+            y_data,
+            "K Nearest Neighbours",
+            KNeighborsClassifier(n_neighbors=self.const["K_val"]),
+        )
 
     def dummy_classifier(self, x_data: dict, y_data: dict, dictionary):
         """
         Function to perform naive classification on our data per subreddit
         """
-        for subreddit in self.config["subreddits"]:
-            x_list = np.array(x_data[subreddit])
-            y_list = np.array(y_data[subreddit])
-            x_train, x_test, y_train, y_test = train_test_split(
-                x_list, y_list, test_size=self.const["test_size"]
-            )
-            model = DummyClassifier(strategy=self.const["dummy_strategy"])
-            model.fit(x_train, y_train)
-            score = cross_val_score(model, x_test, y_test, cv=self.const["cross_val"])
-            logging.info("Dummy Classifier on subreddit %s scored: %s", subreddit, score)
+        self.model_execution(
+            x_data,
+            y_data,
+            "Dummy Classifier",
+            DummyClassifier(strategy=self.const["dummy_strategy"]),
+        )
 
         for sub in x_data.keys():
             x_val = np.array(x_data[sub])
@@ -105,4 +91,3 @@ class DataAnalyser:
                 dictionary[sub][counts.index(highest)],
                 highest,
             )
-
